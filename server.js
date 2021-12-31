9e82e03deca888bf91104a0a74c8e5caf9c69edb
arenignacio <55777461+arenignacio@users.noreply.github.com>
2021-12-31 13:41:27 -0500

    Disable cors for testing purposes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.DB_URL, (err) => {
 });
 
 //#middleware
-app.use(cors());
+//app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash());
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const API_BASEURL = 'http://localhost:8080';
+export const API_BASEURL = 'https://katol-bug-tracker.herokuapp.com';
 //heroku url https://katol-bug-tracker.herokuapp.com
+//local url http://localhost:8080
