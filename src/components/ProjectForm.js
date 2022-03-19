fb5a417c7ef7d00cb50eb8a3c13d3564cdaa3bfd
arenignacio <55777461+arenignacio@users.noreply.github.com>
2022-03-19 18:22:46 -0400

    Add fields layout to ProjectForm

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
 const Container = styled.form`
-	padding: 10px 50px;
+	display: flex;
+	width: 50vw;
+	padding: 25px 50px;
+
+	> div {
+		width: 40%;
+
+		&:first-child {
+			width: 60%;
+		}
+	}
 
 	.row {
 		display: flex;
@@ -9,7 +19,16 @@ const Container = styled.form`
 	}
 
 	.input {
+		width: 40%;
 		margin-bottom: 20px;
+
+		&.project-field {
+			width: 60%;
+		}
+
+		&.desc-field {
+			width: 100%;
+		}
 
 		label {
 			font-size: 0.9rem;
@@ -19,13 +38,20 @@ const Container = styled.form`
 			margin: 5px 15px 0px 0px;
 			padding: 5px;
 			border-radius: 5px;
-			border: 1px solid rgba(0, 0, 0, 0.5);
+			border: 1px solid rgba(0, 0, 0, 0.3);
 			outline: none;
 			font-size: 0.7rem;
+			width: 90%;
 		}
 
 		textarea {
+			width: 95%;
+			height: 125px;
+			outline: none;
+			resize: none;
+			border: 1px solid rgba(0, 0, 0, 0.3);
 			margin: 5px 15px 0px 0px;
+			padding: 5px;
 		}
 	}
 `;
@@ -33,25 +59,28 @@ const Container = styled.form`
 const ProjectForm = () => {
 	return (
 		<Container>
-			<div className="row">
-				<div className="input">
-					<label>Project Name</label>
-					<br />
-					<input type="text" name="" id="" />
+			<div>
+				<div className="row">
+					<div className="input project-field">
+						<label>Project Name</label>
+						<br />
+						<input type="text" name="" id="" />
+					</div>
+					<div className="input">
+						<label> Project Managers</label>
+						<br />
+						<input type="text" name="" id="" />
+					</div>
 				</div>
-				<div className="input">
-					<label> Project Managers</label>
-					<br />
-					<input type="text" name="" id="" />
+				<div className="row">
+					<div className="input desc-field">
+						<label htmlFor="">Description</label>
+						<br />
+						<textarea name="" id="" cols="30" rows="10"></textarea>
+					</div>
 				</div>
 			</div>
-			<div className="row">
-				<div className="input">
-					<label htmlFor="">Description</label>
-					<br />
-					<textarea name="" id="" cols="30" rows="10"></textarea>
-				</div>
-			</div>
+			<div>asda</div>
 		</Container>
 	);
 };
